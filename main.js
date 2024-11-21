e372badad4e3754f8ac94cc741e7b54bff04ac85
Keiran Jensen <keiranjensen@gmail.com>
2024-11-20 21:17:58 -0500

    wip

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import { randomInt } from 'node:crypto';
+import { readdir } from 'node:fs/promises';
+import { OBSWebSocket } from 'obs-websocket-js';
+
+const obs = new OBSWebSocket();
+
+let files = await readdir('./videos');
+const allowed_filetypes = ['.webm', '.mkv'];
+
+files = files.filter(f => {
+  for (const t of allowed_filetypes) {
+    if (f.endsWith(t)) {
+      return true;
+    }
+  }
+  return false;
+});
+
+const file = files[randomInt(files.length)];
+
+await obs.connect('ws://127.0.0.1:4455');
