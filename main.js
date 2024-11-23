4f81761384ee83cd9a80e0148c7505de577781fc
Keiran Jensen <keiranjensen@gmail.com>
2024-11-22 20:24:07 -0500

    prototype

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { randomInt } from 'node:crypto';
+import { realpathSync } from 'node:fs';
 import { readdir } from 'node:fs/promises';
 import { OBSWebSocket } from 'obs-websocket-js';
 
@@ -16,6 +17,34 @@ files = files.filter(f => {
   return false;
 });
 
-const file = files[randomInt(files.length)];
-
 await obs.connect('ws://127.0.0.1:4455');
+
+const input = await obs.call('GetInputSettings', {
+  inputName: 'Correct Horse Battery Staple',
+});
+
+let settingsResp = await obs.call('GetInputDefaultSettings', {
+  inputKind: input.inputKind,
+});
+
+let settings = settingsResp.defaultInputSettings;
+for (const key in input.inputSettings) {
+  settings[key] = input.inputSettings[key];
+}
+
+setInterval(async () => {
+  const status = await obs.call('GetMediaInputStatus', {
+    inputName: 'Correct Horse Battery Staple',
+  });
+
+  if (status['mediaDuration'] === null) {
+    const file = files[randomInt(files.length)];
+
+    settings['local_file'] = realpathSync('./videos/' + file);
+
+    await obs.call('SetInputSettings', {
+      inputName: 'Correct Horse Battery Staple',
+      inputSettings: settings,
+    });
+  }
+}, 5000);
