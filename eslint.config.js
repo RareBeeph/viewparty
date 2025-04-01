85d90cb4a2d7cbbce1df39987194b7346e07a617
Keiran Jensen <keiranjensen@gmail.com>
2025-03-31 20:07:02 -0400

    Address that linter warning and clean up eslint config formatting

--- eslint.config.js
+++ eslint.config.js
@@ -7,23 +7,24 @@ import { join } from 'node:path';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
   {
     rules: {
       'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
-  },
-  {
     languageOptions: {
+      globals: globals.browser,
       parserOptions: {
         projectService: true,
         tsconfigRootDir: join(import.meta.dirname, 'client'),
       },
     },
+    settings: { react: { version: 'detect' } },
   },
+
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat['jsx-runtime'],
+
   { ignores: ['**/*.{js,jsx}'] },
   ...tseslint.configs.recommendedTypeChecked,
 ];
