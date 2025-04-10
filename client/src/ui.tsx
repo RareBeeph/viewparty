26f31f976d05745997640458743549b7dec5f136
Aria Taylor <ari@aricodes.net>
2025-04-10 15:48:50 -0700

    Use destructuring assignment on state, consolidate type check

--- client/src/ui.tsx
+++ client/src/ui.tsx
@@ -3,19 +3,18 @@ import { useContext } from 'react';
 import { SocketContext } from './socketcontext';
 
 const UI = () => {
-  const { state } = useContext(SocketContext);
+  const {
+    state: { inputs, videos },
+  } = useContext(SocketContext);
 
-  const inputs = state.inputs;
-  const videos = state.videos;
-
-  if (typeof inputs == 'string' || typeof videos == 'string') {
-    return;
+  if (![inputs, videos].every(Array.isArray)) {
+    return null;
   }
 
   return (
     <>
-      <SelectForm action="input" options={inputs} />
-      <SelectForm action="next" options={videos} />
+      <SelectForm action="input" options={inputs as string[]} />
+      <SelectForm action="next" options={videos as string[]} />
     </>
   );
 };
